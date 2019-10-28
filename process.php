c4600e457b8decea1a60afce546b0078676a032c
fifon <filip_michalczyk@wp.pl>
2019-10-28 16:26:39 +0100

    connection to db

--- process.php
+++ process.php
@@ -1,3 +1,22 @@
 <?php
 
+    class Todo {
+        protected $conn;
+        
+        // public function __construct(){
+            
+        // }
+        public function connect(){
+            $this->conn = mysqli_connect('localhost', 'fifon', 'test1234', 'todo_list');
+
+            if(!$this->conn){
+                echo 'Connection error: ' . mysqli_connect_error();
+            }
+
+        }
+    }
+
+    $todoOne = new Todo();
+    echo $todoOne->connect();
+
 ?>
